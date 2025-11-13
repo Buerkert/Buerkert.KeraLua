d469bf06fe81a51e75a4c3e5618a5f5fc297f762
Tom Schneider <Tom.Schneider@burkert.com>
2025-11-13 15:01:41 +0100

    remove unused win-arm build config for liblua

--- build/win/arm/toolchain.cmake
+++
@@ -1,8 +0,0 @@
-# the name of the target operating system
-set(CMAKE_SYSTEM_NAME Windows)
-
-set(CMAKE_GENERATOR_PLATFORM "ARM,version=10.0.22621.0" CACHE INTERNAL "")
-
-cmake_policy(SET CMP0011 NEW)
-cmake_policy(SET CMP0091 NEW)
-set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
